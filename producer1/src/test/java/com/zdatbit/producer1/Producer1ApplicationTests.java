b874b9135c9a9ed91f9f670c180a19f73c8da47b
zhangdi <zdatbit@163.com>
2020-12-28 17:27:42 +0800

    springcloud全家桶学习

---
+++ producer1/src/test/java/com/zdatbit/producer1/Producer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zdatbit.producer1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Producer1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
